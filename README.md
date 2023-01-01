4c790bfbb5aacf9ad19c223cf0707b44121836e6
Senopiece <shichiromail@gmail.com>
2023-01-01 18:53:07 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Face-controlled media player
+
+A simple media player controlled by face gestures
---
+++ android/app/src/main/kotlin/me/nabuki/fcmp/MainActivity.kt
@@ -0,0 +1,6 @@
+package me.nabuki.fcmp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
